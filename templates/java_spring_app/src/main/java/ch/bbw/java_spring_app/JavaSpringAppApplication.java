bbd6e819e6fad8bf9c67ee2cd6cbbf799d9d78ee
Stephan <stephanhagmann@outlook.com>
2024-06-28 12:10:52 +0200

    Added the java template

---
+++ templates/java_spring_app/src/main/java/ch/bbw/java_spring_app/JavaSpringAppApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.java_spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringAppApplication.class, args);
+	}
+
+}
---
+++ templates/java_spring_app/src/test/java/ch/bbw/java_spring_app/JavaSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.java_spring_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
